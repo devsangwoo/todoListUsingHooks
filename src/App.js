641d79a1b2818f3f2b15653e773b95358519f740
Sangwoo Cho <51525414+devsangwoo@users.noreply.github.com>
2020-02-02 18:07:41 +0900

    [react]
    stateHooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+const Todo = ({ todo, index, completeTodo, removeTodo }) => {
+  return <div />;
+};
 const App = () => {
   const [todo, setTodo] = useState([]);
 
   return (
     <div className="app">
       <div className="todo-list" />
+      {todo.map()}
     </div>
   );
 };
